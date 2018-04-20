60e70b1a2420180f1d396b1a642d19287dd991a2
Caleb Curry <clbcrry@hotmail.com>
2018-04-20 14:22:02 -0500

    completed exercise 2

--- AlgorithmAnalysis/MinesweeperJava/src/MinesweeperJava.java
+++ AlgorithmAnalysis/MinesweeperJava/src/MinesweeperJava.java
@@ -5,132 +5,84 @@ import java.util.ArrayList;
 
 public class MinesweeperJava {
 
-	
-	/*
-	rows = []
-
-			print("first, we will take manual input:")
-
-			height = input()
-			width = input()
-
-			for i in range(0, int(height)):
-			    rows.append(input())
-
-			count = 0
-			for i in range(0, int(height)):
-			    #print("i:", i)
-			    for y in range(0, int(width)):
-			        if(rows[i][y] == "*"):
-			            print('*', end='')
-			            continue
-			        if(i == 0):
-			            if(y > 0):
-			                if(rows[i][y-1] == '*'):
-			                    count = count + 1
-			            if(y < int(width)-1):
-			                if(rows[i][y+1] == '*'):
-			                    count = count + 1
-			                    if (count > 0):
-			                        print(count, end='')
-			                    else:
-			                        print('teret ', end='')
-			                    
-			            #print(count, end='')
-			        elif(i > 0 and i < int(height)-1):
-			            1 + 1
-			            #print("middle row  ", end='')
-			        else:
-			            1 + 1
-			            #print("last row  ", end='')
-			    
-			print('\n', end='')
-			print(rows)
-
-*/
+	public static char exists(char[][] rows, int row, int column)
+	{
+		if(row < 0 || column < 0 || row == rows.length || column == rows[0].length)
+		{
+			return 'C';	
+		} else 
+		{ 
+			return rows[row][column];
+		}
+	}
 	
 	public static void main(String[] args) throws IOException {
 		System.out.println("let's input");
-		ArrayList<String> rows = new ArrayList<String>();
+		//ArrayList<String> rows = new ArrayList<String>();
 		BufferedReader reader = new BufferedReader( new InputStreamReader( System.in ) );
-		String input = new String();
-		String height = reader.readLine();
-		String width = reader.readLine();
+		int height = Integer.parseInt(reader.readLine());
+		int width = Integer.parseInt(reader.readLine());
 		
+		char[][] rows = new char[height][width];
 		
-		for(int i = 0; i <  Integer.parseInt(height); i++)
+		for(int i = 0; i < height; i++)
 		{
-			rows.add(reader.readLine());
+			String row = reader.readLine();
+			
+			for (int j = 0; j < width; j++)
+			{
+				rows[i][j] = row.charAt(j);
+			}
 		}
 		
+		for(int i = 0; i < height; i++)
+		{	
+			for (int j = 0; j < width; j++)
+			{
+				System.out.print(rows[i][j]);
+			}
+
+			System.out.println("");
+		}
+		
+		System.out.println("~~~~~");
 		
 		
 		int count = 0;
 				
-		for (int i = 0; i < Integer.parseInt(height); i++)
+		for (int i = 0; i < height; i++)
 		{
-			for (int y = 0; y < Integer.parseInt(width); y++)
+			for (int j = 0; j < width; j++)
 			{
-				if (rows.get(i).charAt(y) == '*')
+				if(rows[i][j] != '*')
 				{
-					continue;
-				}
-				else
-				{
-					if(i == 0) //first row
+					if(exists(rows,i-1, j-1) == '*') count++;
+					if(exists(rows, i-1, j) == '*') count++;
+					if(exists(rows, i-1, j + 1) == '*') count++;
+					if(exists(rows, i, j-1) == '*') count++;
+					if(exists(rows, i, j+1) == '*') count++;
+					if(exists(rows, i+1, j-1) == '*') count++;
+					if(exists(rows, i+1, j) == '*') count++;
+					if(exists(rows, i+1, j+1) == '*') count++;
+				
+					if (count != 0)
 					{
-						
-						System.out.println("first row");
-						
-						if(y == 0)
-						{
-							if(rows.get(i).charAt(y) == '*')
-							{
-								count++;
-							}
-						}
-						else if (y > 0 && y < Integer.parseInt(width) - 1)
-						{
-							if(rows.get(i).charAt(y-1) == '*')
-							{
-								count++;
-							}
-							if(rows.get(i).charAt(y) == '*')
-							{
-								count++;
-							}
-							
-						} else
-						{
-							if(rows.get(i).charAt(y-1) == '*')
-							{
-								count++;
-							}
-						}
-						
-						String taco = "hello";
-						taco[5] = 'h'
-						rows.get(i).charAt(y) = count;
-						count = 0;
+						Character theNumber = Character.forDigit(count, 10);
+						rows[i][j] = theNumber;
 					}
-					else if (i > 0 && i < Integer.parseInt(height) - 1)
-					{
-						//middle row
-						System.out.println("middle row");
-					}
-					else
-					{
-						//bottom row
-						System.out.println("bottom row");
-					}
+					count = 0;
 				}
 			}
 		}
-		
-		
-		
-				        
+		for(int i = 0; i < height; i++)
+		{	
+			for (int j = 0; j < width; j++)
+			{
+				System.out.print(rows[i][j]);
+			}
 
+			System.out.println("");
+		}
 	}
 
 }
